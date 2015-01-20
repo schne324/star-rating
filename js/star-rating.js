40f5fbfa94d4c50b0163977aaa2344cb12f6b5f9
Harris Schneiderman <harris.schneiderman@deque.com>
2015-01-20 16:09:34 -0500

    adds arrow key support

--- js/star-rating.js
+++ js/star-rating.js
@@ -4,12 +4,22 @@ var $star = $('#star');
 
 // initial setup:
 $star.find('ul').each(function (i, ul) {
+  // find the tip and give it an id that we can point to (aria-describedby)
+  var tipID = rndid('hms-', 7);
+  $(ul.parentNode).find('.tip').prop('id', tipID);
+
+  var $h2 = $(ul).closest('.rate-me').find('h2');
+  $h2.prop('id', rndid('h-', 7));
+  ul.setAttribute('role', 'radiogroup');
+  ul.setAttribute('aria-labelledby', $h2.prop('id'));
   // iterating through each ul and THEN each li within the
   // given ul to leverage `i` (index) for data-attributes
   $(ul).find('li').each(function (i, li) {
     $(li).attr({
-      'role': 'button',
-      'data-star-index': i
+      'role': 'radio',
+      'data-star-index': i,
+      'aria-checked': 'false',
+      'aria-labelledby': tipID
     });
 
     if (0 === i) {
@@ -17,25 +27,58 @@ $star.find('ul').each(function (i, ul) {
     } else {
       li.tabIndex = -1;
     }
+
     $(li).on('focus', focusHandler);
+    $(li).on('blur', blurHandler);
   });
+
 });
 
 // mouse users:
-$star.on('click', '.five-stars li', function () {
+$star.on('click', '.five-stars li', function (e) {
+  e.preventDefault();
   ratingConfig(this);
 });
 
 // keyboard users:
-// $star.on('keydown', '.five-stars li', function (keyVent) {
+$star.on('keydown', '.five-stars li', function (keyVent) {
+  var which = keyVent.which;
+  var target = keyVent.target;
 
-// });
+  switch (which) {
+
+    case 37: // LEFT
+      keyVent.preventDefault(); // don't scroll
+      keyMotion(target, 'prev');
+      break;
+    case 38: // UP
+      keyVent.preventDefault();
+      keyMotion(target, 'next');
+      break;
+    case 39: // RIGHT
+      keyVent.preventDefault();
+      keyMotion(target, 'next');
+      break;
+    case 40: // DOWN
+      keyVent.preventDefault();
+      keyMotion(target, 'prev');
+  }
+});
 
 function focusHandler() {
-  var isChecked = $(this).hasClass(fullStar);
   var numStars = parseInt(this.getAttribute('data-star-index'), 10) + 1;
   var $tip = $(this).closest('.five-stars').find('.tip').first();
   $tip.html(numStars + '/5 stars').show();
+}
+
+function blurHandler() {
+  var starIndex = parseInt(this.getAttribute('data-star-index'), 10);
+  var isChecked = $(this).hasClass(fullStar);
+
+  if (starIndex === 0 && !isChecked) {
+    var $tip = $(this).closest('.five-stars').find('.tip').first();
+    $tip.html('not yet rated');
+  }
 }
 
 function fill(star) {
@@ -77,8 +120,60 @@ function setActiveStar(star) {
     .each(function (i, li) {
       if (li === star) {
         li.tabIndex = 0;
+        $(li).attr('aria-checked', 'true');
       } else {
         li.tabIndex = -1;
+        $(li).attr('aria-checked', 'false');
       }
     });
+}
+
+
+function keyMotion(target, dir) {
+  var currentIndex = parseInt($(target).attr('data-star-index'), 10);
+  var $stars = $(target).closest('ul').find('li');
+  var adjacentStar = (dir === 'prev') ? $stars[currentIndex - 1]
+                      : $stars[currentIndex + 1];
+  if (adjacentStar) {
+    adjacentStar.click();
+    adjacentStar.focus();
+  }
+}
+
+///////////
+// RNDID //
+///////////
+// @credit: Stephen Mathieson
+// https://github.com/stephenmathieson/rndid
+function rndid(prefix, length) {
+  if ('number' == typeof prefix)
+    length = prefix, prefix = '';
+  length = length || exports.defaultLength;
+  var id = (prefix || '') + str(length);
+  if (document.getElementById(id)) return rndid(prefix, length);
+  return id;
+}
+
+/**
+ * Generate a random alpha-char.
+ *
+ * @api private
+ * @return {String}
+ */
+
+function character() {
+  return String.fromCharCode(Math.floor(Math.random() * 25) + 97);
+}
+
+/**
+ * Generate a random alpha-string of `len` characters.
+ *
+ * @api private
+ * @param {Number} len
+ * @return {String}
+ */
+
+function str(len) {
+  for (var i = 0, s = ''; i < len; i++) s += character();
+  return s;
 }
